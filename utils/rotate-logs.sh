5612ecf5f28ed5d23dae6a85fb0b2db6e21ccf30
Matti Aarnio <mea@nic.funet.fi>
1998-02-15 04:37:10 +0000

    Improved "boiler-plate" approach, does it my uses...

--- utils/rotate-logs.sh
+++ utils/rotate-logs.sh
@@ -1,18 +1,43 @@
 #!/bin/sh
 #
+#  Log-rotation utility for ZMailer
 #
+#  Your mileage may vary as you tune your loggings
+#  At nic.funet.fi we use this DAILY, elsewere it
+#  can be used weekly to monthly...
+#
+
+# ZCONFIG=@ZMAILERCFGFILE@
+# . $ZCONFIG
 
 . /etc/zmailer.conf
 
 cd $LOGDIR
 
-for x in router scheduler smtpserver smtp
+FILES="router scheduler smtpserver smtp"
+
+for x in $FILES
 do
-	for y in oooo ooo oo o
+	# cut, and compress
+	for y in 8 7 6 5 4 3 2 1 0
 	do
-		/bin/mv $x.$y $x.${y}o
+		z=`expr $y + 1`
+		if [ -f $x.$y.gz ]; then
+		    /bin/mv $x.$y.gz $x.$z.gz
+		fi
 	done
-	/bin/mv	$x $x.o
+	if [ -f $x ]; then
+		/bin/mv	$x $x.0
+		/bin/touch $x
+	fi
 done
 
-/etc/zmailer  # restart them all
+$MAILBIN/zmailer logsync
+
+# Sleep here to let the logsync to take effect
+#  -- what ? half an hour ?
+sleep 1800
+
+# Compress the newly separated log files
+gzip *.0
+
