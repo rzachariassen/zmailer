bd7f480194cb300a5f29d71ddd9bae82685c5341
Matti Aarnio <mea@nic.funet.fi>
1998-04-20 16:18:16 +0000

    New year, new numbers

--- lib/prversion.c
+++ lib/prversion.c
@@ -4,7 +4,7 @@
  */
 /*
  *	A lot of changes all around over the years by Matti Aarnio
- *	<mea@nic.funet.fi> (and elsewere), copyright 1992-1997
+ *	<mea@nic.funet.fi> (and elsewere), copyright 1992-1998
  */
 
 #include "mailer.h"
@@ -12,7 +12,7 @@
 #include "libsh.h"
 
 static const char *Copyright = "Copyright 1992 Rayan S. Zachariassen\n\
-Copyright 1992-1997 Matti Aarnio";
+Copyright 1992-1998 Matti Aarnio";
 
 void
 prversion(prgname)
@@ -20,4 +20,6 @@ prversion(prgname)
 {
 	fprintf(stderr, "ZMailer %s (%s)\n  %s:%s\n%s\n", prgname,
 		Version, CC_user, CC_pwd, Copyright);
+	fprintf(stderr, "Configured with command: '%s'\n",
+		CONFIGURE_CMD);
 }
