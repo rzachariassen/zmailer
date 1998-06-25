77b4d30212e5ff4155f71c408bc75451f27acefe
Matti Aarnio <mea@nic.funet.fi>
1998-06-25 20:37:59 +0000

    Some fixes for MD5 compilation at most systems. Solaris is to be checked still..

--- include/md5.h
+++ include/md5.h
@@ -34,9 +34,6 @@ typedef struct _MD5_CTX {
 } MD5_CTX;
 
 void   MD5Init (MD5_CTX *);
-void   MD5Update (MD5_CTX *, const unsigned char *, unsigned int);
+void   MD5Update (MD5_CTX *, const unsigned char *, const unsigned int);
 void   MD5Final (unsigned char [16], MD5_CTX *);
-char * MD5End(MD5_CTX *, char *);
-char * MD5File(char *, char *);
-char * MD5Data(const unsigned char *, unsigned int, char *);
 #endif /* _MD5_H_ */
