b0a6c9dc64430d72fd50ad2d170659372a46045e
Matti Aarnio <mea@nic.funet.fi>
1998-08-02 22:36:45 +0000

    The SunOS 4.1.* compiler is un-f*-believable ... ... or then I have forgotten how horrible the pre-ANSI-C era compilers were...

--- libsh/shconfig.h
+++ libsh/shconfig.h
@@ -21,12 +21,13 @@
 #define	DEFAULT_ROOT_PS1	"# "
 #define	DEFAULT_IFS		" \t\n"
 #define	DEFAULT_PATH		"/usr/ucb:/bin:/usr/bin:"
-#define	LOGIN_SCRIPT		"if [ -f /etc/profile ]; then\n" \
-				"	. /etc/profile;\n" \
-				"fi;\n" \
-				"if [ -f $HOME/.profile ]; then\n" \
-				"	. $HOME/.profile;\n" \
-				"fi\n"
+#define	LOGIN_SCRIPT \
+if [ -f /etc/profile ]; then\n\
+	. /etc/profile;\n\
+fi;\n\
+if [ -f $HOME/.profile ]; then\n\
+	. $HOME/.profile;\n\
+fi\n"
 
 /* standard variables */
 #define	CDPATH			"CDPATH"
