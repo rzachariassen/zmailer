da4237a3e68f6cd122e8b3b8d3de8220f38b84e1
Matti Aarnio <mea@nic.funet.fi>
1999-12-30 17:43:22 +0000

    verbose logging omitted one newline - log looked "weird"..

--- router/rfc822.c
+++ router/rfc822.c
@@ -2273,6 +2273,7 @@ sequencer(e, file)
 				    fprintf(vfp, "%d ", rcp->urw.number);
 				  prctladdr(rcp->info, vfp,
 					    _CF_RECIPIENT, "recipient");
+				  putc('\n', vfp);
 				  /* DSN data output ! */
 				  prdsndata(rcp->info, vfp,
 					    _CF_RCPTNOTARY, "recipient");
