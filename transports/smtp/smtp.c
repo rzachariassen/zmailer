82fedd0944d1aa302c80df9704d3b8b6982ce80c
Matti Aarnio <mea@nic.funet.fi>
2000-02-02 21:05:27 +0000

    Lengthened the smtp's -Ttcpw= default timeout to 5 minutes from its RFC 1123 value of 3 minutes.

--- transports/smtp/smtp.c
+++ transports/smtp/smtp.c
@@ -12,13 +12,14 @@
      - DATA initialization (until "354.."): 2 minutes
      - While writing data, a block
        at the time: 3 minutes  (How large a block ?)
+       (We increased this to 5 minutes)
      - From "." to "250 OK": 10 minutes
        (We use 20 minutes here - sendmail uses 60 minutes..)
  */
 int timeout = 0;		/* how long do we wait for response? (sec.) */
 int timeout_cmd  =  5*60;
 int timeout_data =  2*60;
-int timeout_tcpw =  3*60;	/* All tcp writes ?? */
+int timeout_tcpw =  5*60;	/* All tcp writes ?? */
 int timeout_dot  = 20*60;
 int timeout_conn =  3*60;	/* connect() timeout */
 
