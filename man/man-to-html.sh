d1d740b38b109c27c05e9d7c4af1c1619fc14e2b
Matti Aarnio <mea@nic.funet.fi>
2003-08-28 11:07:59 +0000

    Apparently we can be without 'groff -c' option, and still not get colour output to produced HTML..   Oh yes, that was when using '-Thtml' output driver.  Present HTML generation uses '-Tascii' and magic state processor to convert produced (with occasional escapes) text file into fixed with courier font HTML.

--- man/man-to-html.sh
+++ man/man-to-html.sh
@@ -18,7 +18,7 @@ export LINES
 echo "<HTML><HEAD><TITLE>"
 basename "$1"
 echo "</TITLE></HEAD><BODY BGCOLOR=white><PRE>"
-groff  -c -t -man -Tascii "$1" | \
+groff  -t -man -Tascii "$1" | \
     perl -e '
         #select STDIN; $| = 1;
         select STDERR; $| = 1;
